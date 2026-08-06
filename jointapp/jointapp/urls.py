c42cd577e2babce4d81010fa7461430ffbacc0be
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-08-06 12:26:28 +0200

    modular URL link

--- jointapp/jointapp/urls.py
+++ jointapp/jointapp/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from joint import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('joint/', views.joint),
+    path('joint/', include('joint.urls')),
 ]
