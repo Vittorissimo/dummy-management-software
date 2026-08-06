c7e8609ea35987f2c93ae18cf257c4982d996f88
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-08-06 11:18:03 +0200

    added joint site and fix bug

--- jointapp/joint/views.py
+++ jointapp/joint/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def joint(request):
-    return render(request, 'joint/jointGui')    
+    return render(request, 'joint/jointGui.html')    
